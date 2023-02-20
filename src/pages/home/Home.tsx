f95e205371dc50da84a2b847af769ee6dd5aa9b2
kaasikodes <thisbroisfresh@gmail.com>
2023-02-20 12:48:29 +0100

    home/added img to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,15 +5,20 @@ import { appRoutes } from "data/routes";
 const Home = () => {
   return (
     <div className="mt-6 flex flex-col items-stretch gap-4">
-      <h4 className="text-2xl text-center">
+      <h4 className="text-2xl text-center ">
         Are you are an introvert or extrovert?
       </h4>
-      <p className="text-center">
+      <img
+        src={"assets/personality.jpg"}
+        alt={"personality"}
+        className="object-contain h-52"
+      />
+      <p className="text-center text-sky-400">
         Find out what is personality by taking a series of tests that have been
         curated by the best doctors worldwide
       </p>
       <div className="flex justify-center w-full">
-        <Button size="large" href={appRoutes.start}>
+        <Button size="large" href={appRoutes.start} type="primary">
           Start Test
         </Button>
       </div>
