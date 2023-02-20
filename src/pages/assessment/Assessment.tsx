6eeab82e7b5cdb7c1d11bf3ec4a103942a68c9fe
kaasikodes <thisbroisfresh@gmail.com>
2023-02-20 23:57:02 +0100

    assessment/created assessment page

--- src/pages/assessment/Assessment.tsx
+++ src/pages/assessment/Assessment.tsx
@@ -1,9 +1,12 @@
+import QuestionsContainer from "components/question";
+
 export const headerText = "Are you are an introvert or extrovert?";
 
 const Assessment = () => {
   return (
     <div className="flex flex-col gap-6">
       <h4 className="text-2xl text-center">{headerText}</h4>
+      <QuestionsContainer />
     </div>
     // Display the questions
   );
