3328e7e87bb1f0321a1d5643c73b9c225ab5734f
kaasikodes <thisbroisfresh@gmail.com>
2023-02-20 12:50:03 +0100

    layout/wrote test

---
+++ src/components/layout/Layout.test.tsx
@@ -0,0 +1,11 @@
+import { appRoutes } from "data/routes";
+import { screen, render } from "test-utils";
+import Layout from "./Layout";
+
+describe("Layout", () => {
+  test("Ensure teamway assessment text is in the document", () => {
+    render(<Layout />);
+    const comp = screen.getByText("Team way Assessment");
+    expect(comp).toBeInTheDocument();
+  });
+});
