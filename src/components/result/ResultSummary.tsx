1dbef7173884b7b1bb9dee102c34db2b1b6afcd6
kaasikodes <thisbroisfresh@gmail.com>
2023-02-21 00:29:31 +0100

    result/added a comment explaining how to determine personality

--- src/components/result/ResultSummary.tsx
+++ src/components/result/ResultSummary.tsx
@@ -7,6 +7,13 @@ interface IProps {
 }
 
 const ResultSummary = ({ introVal, extroVal }: IProps) => {
+  // The concept behind determining wether person is introverted or extroverted is as follows:
+  // Each option in a question is indicative of how introverted or extroverted an individual is
+  // i.e introVal, & extroVal (these values are created in such a way that there sum is always 5)
+  // the sum of the values is always 5 to ensure that at the end of the assesment, the individual
+  // is either introverted or extroverted as 5 cannot be shared evenly
+  // provided the sum of these 2 values are 5, the individual will always be one or the other but never both
+  // As a result of this constraint one value will always be greater than the other
   return (
     <>
       <h4 className="text-2xl text-center">
